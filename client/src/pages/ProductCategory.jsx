814163aae40bad3c6513fa154db8e497ce1bcba0
vj4614 <vanshjaiswal20.06.06@gmail.com>
2025-08-08 01:06:26 +0530

    all button working

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -11,7 +11,7 @@ const ProductCategory = () => {
 
     const searchCategory = categories.find((item)=> item.path.toLowerCase() === category)
 
-    const filteredProducts = products.filter((product)=>product.category.toLowerCase() === category)
+    const filteredProducts = products.filter((product)=>product.category.toLowerCase() === category.toLowerCase())
 
   return (
     <div className='mt-16 pb-16'>
