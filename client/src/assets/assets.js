9f81dbedc0ece6be8359699ebdbf28623ff5bdfd
vj4614 <vanshjaiswal20.06.06@gmail.com>
2025-08-09 18:47:35 +0530

    cleaning of code, removed unnesary functionality and lines of code

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,30 +1,5 @@
 import logo from "./logo.svg";
-import search_icon from "./search_icon.svg";
-import remove_icon from "./remove_icon.svg";
-import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
-import star_icon from "./star_icon.svg";
-import star_dull_icon from "./star_dull_icon.svg";
-import cart_icon from "./cart_icon.svg";
-import nav_cart_icon from "./nav_cart_icon.svg";
-import add_icon from "./add_icon.svg";
-import refresh_icon from "./refresh_icon.svg";
-import product_list_icon from "./product_list_icon.svg";
-import order_icon from "./order_icon.svg";
 import upload_area from "./upload_area.png";
-import profile_icon from "./profile_icon.png";
-import menu_icon from "./menu_icon.svg";
-import delivery_truck_icon from "./delivery_truck_icon.svg";
-import leaf_icon from "./leaf_icon.svg";
-import coin_icon from "./coin_icon.svg";
-import box_icon from "./box_icon.svg";
-import trust_icon from "./trust_icon.svg";
-import black_arrow_icon from "./black_arrow_icon.svg";
-import white_arrow_icon from "./white_arrow_icon.svg";
-import main_banner_bg from "./main_banner_bg.png";
-import main_banner_bg_sm from "./main_banner_bg_sm.png";
-import bottom_banner_image from "./bottom_banner_image.png";
-import bottom_banner_image_sm from "./bottom_banner_image_sm.png";
-import add_address_iamge from "./add_address_image.svg";
 import main_banner from "./main_banner.png";
 import living_room from "./living_room.png";
 import kitchen from "./kitchen.png";
@@ -40,7 +15,7 @@ import craftsmanship_icon from "./craftsmanship_icon.svg";
 import sustainable_icon from "./sustainable_icon.svg";
 import assembly_icon from "./assembly_icon.svg";
 import subtle_pattern from "./subtle_pattern.svg";
-
+import box_icon from "./box_icon.svg";
 import moodboard_bg from "./moodboard_bg.png"; 
 
 
@@ -54,32 +29,8 @@ export const assets = {
   decore,
   outdoor,
   kid_room,
-  search_icon,
   main_banner,
-  remove_icon,
-  arrow_right_icon_colored,
-  star_icon,
-  star_dull_icon,
-  cart_icon,
-  nav_cart_icon,
-  add_icon,
-  refresh_icon,
-  product_list_icon,
-  order_icon,
   upload_area,
-  profile_icon,
-  menu_icon,
-  delivery_truck_icon,
-  leaf_icon,
-  coin_icon,
-  trust_icon,
-  black_arrow_icon,
-  white_arrow_icon,
-  main_banner_bg,
-  main_banner_bg_sm,
-  bottom_banner_image,
-  bottom_banner_image_sm,
-  add_address_iamge,
   box_icon,
   design_icon,
   craftsmanship_icon,
