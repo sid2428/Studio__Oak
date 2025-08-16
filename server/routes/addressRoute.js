732676056f3afcd1b6afc82c328c4745777b32eb
Kartik_2428 <kartikgaikwad2828@gmail.com>
2025-08-16 11:30:59 +0530

    Update addressRoute.js

--- server/routes/addressRoute.js
+++ server/routes/addressRoute.js
@@ -7,4 +7,4 @@ const addressRouter = express.Router();
 addressRouter.post('/add', authUser, addAddress);
 addressRouter.get('/get', authUser, getAddress);
 
-export default addressRouter;
+export { addressRouter };
