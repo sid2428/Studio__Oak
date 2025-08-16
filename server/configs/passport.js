857412d7d1eb3182320d2adf9b042de3d4a1eb05
Kartik_2428 <kartikgaikwad2828@gmail.com>
2025-08-16 11:20:15 +0530

    Update passport.js

--- server/configs/passport.js
+++ server/configs/passport.js
@@ -3,12 +3,12 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
 
 export default function(passport) {
- passport.use(new GoogleStrategy({
+  passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://studio-oak-frontend.onrender.com/api/user/auth/google/callback',
+    callbackURL: `${process.env.BACKEND_URL}/api/user/auth/google/callback`,
     scope: ['profile', 'email']
-},
+  },
 
     async (accessToken, refreshToken, profile, done) => {
         try {
